1e04f82798278d81c11f3b87d493d5afb2db7831
maibaouyen092017-star <maibaouyen092017@gmail.com>
2025-10-12 22:00:38 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,21 @@
-
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
+
+// Hash password trước khi lưu
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Kiểm tra mật khẩu
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model("User", userSchema);
