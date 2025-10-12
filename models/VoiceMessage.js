b16cee99a8d9f7d18f9edc68ad56fed25dabb041
maibaouyen092017-star <maibaouyen092017@gmail.com>
2025-10-12 21:07:07 +0700

    Update VoiceMessage.js

--- models/VoiceMessage.js
+++ models/VoiceMessage.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
-const voiceSchema = new mongoose.Schema({
-  sender: String,
-  device_id: String,
-  message: String,
-  file_url: String,
-  timestamp: { type: Date, default: Date.now },
-  played: { type: Boolean, default: false }
+const VoiceMessageSchema = new mongoose.Schema({
+  title: String,
+  time: Date,
+  file: String,
+  sent: { type: Boolean, default: false }
 });
 
-export default mongoose.model("VoiceMessage", voiceSchema);
+export default mongoose.model("VoiceMessage", VoiceMessageSchema);
