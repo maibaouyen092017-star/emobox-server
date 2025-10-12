daa96cdc50f5116e7f5afd035dcfc9a581596254
maibaouyen092017-star <maibaouyen092017@gmail.com>
2025-10-12 20:20:39 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,22 @@
 
+import express from "express";
+import User from "../models/User.js";
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  const exists = await User.findOne({ email });
+  if (exists) return res.status(400).json({ msg: "Email đã tồn tại" });
+  const user = new User({ name, email, password });
+  await user.save();
+  res.json({ msg: "Đăng ký thành công" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) return res.status(400).json({ msg: "Sai thông tin đăng nhập" });
+  res.json({ msg: "Đăng nhập thành công", user });
+});
+
+export default router;
