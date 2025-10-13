06d8faf7700717669289a0a69685d268700b209e
maibaouyen092017-star <maibaouyen092017@gmail.com>
2025-10-13 13:03:07 +0700

    Create Alarm.js

---
+++ models/Alarm.js
@@ -0,0 +1,12 @@
+// models/Alarm.js
+import mongoose from "mongoose";
+
+const alarmSchema = new mongoose.Schema({
+  title: { type: String, default: "" },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  fileUrl: { type: String, required: true },
+});
+
+const Alarm = mongoose.model("Alarm", alarmSchema);
+export default Alarm;
